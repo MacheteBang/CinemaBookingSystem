90576dfc7bc3f8725f8e068ebce8f136aa531c0b
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-02-08 13:35:51 -0500

    ⚙️ Scaffold Serilog

--- src/CinemaBooking.WebApi/Program.cs
+++ src/CinemaBooking.WebApi/Program.cs
@@ -1,7 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration)
+);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
