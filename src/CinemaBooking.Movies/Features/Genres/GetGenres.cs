d95c8d3b16a6738170f504e9cdaa148675b66f86
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-02-10 09:15:23 -0500

    🪅 Add `GetGenres` feature

---
+++ src/CinemaBooking.Movies/Features/Genres/GetGenres.cs
@@ -0,0 +1,31 @@
+namespace CinemaBooking.Movies.Features.Genres;
+
+public static class GetGenres
+{
+    public class Query : IRequest<IResult> { }
+
+    internal sealed class Handler : IRequestHandler<Query, IResult>
+    {
+        public async Task<IResult> Handle(Query request, CancellationToken cancellationToken)
+        {
+            ICollection<string> genres = Enum.GetValues(typeof(Genre))
+                .Cast<Genre>()
+                .Select(e => e.ToString())
+                .ToList();
+
+            return await Task.FromResult(Results.Ok(genres));
+        }
+    }
+}
+
+public class GetGenresEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("genres", async (ISender sender) =>
+        {
+            return await sender.Send(new GetGenres.Query());
+        })
+        .WithName(nameof(GetGenres));
+    }
+}
