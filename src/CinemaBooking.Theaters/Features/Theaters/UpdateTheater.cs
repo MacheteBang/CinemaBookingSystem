df45973a7c96acf0b8a24ebf5db65dde90118362
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-02-12 15:18:38 -0500

    🪅 Add `UpdateTheater` feature

---
+++ src/CinemaBooking.Theaters/Features/Theaters/UpdateTheater.cs
@@ -0,0 +1,92 @@
+namespace CinemaBooking.Theaters.Features.Theaters;
+
+public static class UpdateTheater
+{
+    public class Command : IRequest<Result>
+    {
+        public required Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string SeatingArrangement { get; set; } = string.Empty;
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.SeatingArrangement).NotNull();
+        }
+    }
+
+    internal sealed class Handler : IRequestHandler<Command, Result>
+    {
+        private readonly IValidator<Command> _validator;
+        private readonly TheatersDbContext _dbContext;
+
+        public Handler(IValidator<Command> validator, TheatersDbContext dbContext)
+        {
+            _validator = validator;
+            _dbContext = dbContext;
+        }
+
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            ValidationResult validationResult = _validator.Validate(request);
+            if (!validationResult.IsValid)
+            {
+                return TheaterErrors.Validation(validationResult.Errors.Select(e => e.ErrorMessage));
+            }
+
+            Theater theater = new()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                SeatingArrangement = request.SeatingArrangement
+            };
+
+            _dbContext.Theaters.Update(theater);
+            _dbContext.SaveChanges();
+
+            return await Task.FromResult(Result.Success());
+        }
+    }
+}
+
+public class UpdateTheaterEndpoint : IEndpoint
+{
+    public record Request(
+    string Name,
+    string SeatingArrangement
+);
+
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("theaters/{id:guid}", async (Guid id, Request request, ISender sender) =>
+        {
+            var result = await sender.Send(request.ToCommand(id));
+
+            return result.IsSuccess ? Results.Accepted()
+                : result.Error.Code switch
+                {
+                    TheaterErrors.Codes.Invalid => Results.BadRequest(result.Error.Messages),
+                    _ => Results.BadRequest()
+                };
+        })
+        .WithName(nameof(UpdateTheaterEndpoint))
+        .WithTags("Theaters");
+    }
+}
+
+public static class UpdateTheaterMapper
+{
+    public static UpdateTheater.Command ToCommand(this UpdateTheaterEndpoint.Request request, Guid id)
+    {
+        return new()
+        {
+            Id = id,
+            Name = request.Name,
+            SeatingArrangement = request.SeatingArrangement
+        };
+    }
+}
